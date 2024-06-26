3deb3bc64ec4d878d040cff35b5752ad2fb782d2
?? <91481987+4SV322@users.noreply.github.com>
2024-06-26 11:29:59 +0800

    Some changes
    Just logs next step is init app

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"gRPCLearn/iternal/config"
 	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting app",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+	log.Error("error message")
+	log.Warn("warn message")
 
 }
 
@@ -19,6 +35,18 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New()
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
+
+	return log
 }
